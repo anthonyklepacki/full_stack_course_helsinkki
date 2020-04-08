304eb7d87bb95628c94c0aa2659d89b3a4848567
Anthony Klepacki <anthony.klepacki@gmail.com>
2020-04-08 16:20:35 -0500

    Completed 2.1

---
+++ part2/course_info/src/index.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = (props) => {
+  const {course} = props
+  return (
+    <>
+      <Header course={course.name} />
+        {course.parts.map(parts => 
+          <p key={parts.id}>
+            {parts.name} {parts.exercises}
+          </p>
+        )}
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content  = (props) => {
+  return (
+    <>
+      <Part 
+      name={props.parts[0].name} 
+      num={props.parts[0].exercises}/>
+
+     <Part 
+     name={props.parts[1].name} 
+     num={props.parts[1].exercises}/>
+    <Part 
+    name={props.parts[2].name} 
+    num={props.parts[2].exercises}/>
+   </>
+  )
+}
+
+
+const Total = (props) => {
+  const sum = (p1, p2, p3) => {
+    return p1 + p2 + p3
+  }
+  const result = sum(props.parts[0].exercises,props.parts[1].exercises,props.parts[2].exercises)
+  return (
+    <>
+      <p>Number of exercises {result}</p>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+       <p>{props.name} {props.num}</p>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
