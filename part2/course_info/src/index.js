e36dcc2966473e73701ed096675ab93a090f553d
Anthony Klepacki <anthony.klepacki@gmail.com>
2020-04-08 16:56:04 -0500

    Completed 2.4

--- part2/course_info/src/index.js
+++ part2/course_info/src/index.js
@@ -2,46 +2,71 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }      
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(tempCourse => 
+        <Course key={tempCourse.id} name={tempCourse.name} parts={tempCourse.parts}/>
+      )}
+
     </div>
   )
 }
 
 const Course = (props) => {
-  const {course} = props
   return (
     <>
-      <Header course={course.name} />
-        {course.parts.map(parts => 
-          <p key={parts.id}>
-            {parts.name} {parts.exercises}
-          </p>
-        )}
-      <Total parts={course.parts} />
+      <Header course={props.name} />
+
+      {props.parts.map(tempPart => 
+        <Part key={tempPart.id} name={tempPart.name} num={tempPart.exercises}/>
+      )}
+
+      <Total parts={props.parts} />
     </>
   )
 }
